4cd4d7fc6d38bfb0edf13ba2647541d1cef558ac
Ammon Burgi <ammonburgi@Ammons-MacBook-Pro.local>
2020-08-24 19:21:35 -0600

    mongo

--- db/seed.sql
+++ db/seed.sql
@@ -3,4 +3,19 @@ user_id serial primary key,
 email varchar(200),
 username varchar(100),
 password varchar(800)
-)
+);
+
+create table ecom_brand(
+brand_id serial primary key,
+brand_title varchar(300),
+product_type varchar(400)
+);
+
+create table ecom_product(
+product_id serial primary key,
+pro_title varchar(300),
+description text,
+price integer,
+pro_img text,
+brand_id integer REFERENCES ecom_brand(brand_id)
+);
